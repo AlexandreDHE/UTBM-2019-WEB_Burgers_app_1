b723acd60f27cbb7c727936791f961ba4758142d
DELAHOUSSE <delahousse.alex@gmail.com>
2020-03-11 11:37:05 +0100

    Add nil exception

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,7 +1,11 @@
 class Burger < ApplicationRecord
     def nutriments(code)
         @product = Openfoodfacts::Product.get(code, locale: 'fr')
-        @json = @product.nutriments.to_hash
+        if (@product == nil) {
+            @json = {"No data" =>"No data"}
+        } else {
+            @json = @product.nutriments.to_hash
+        }
         return @json
     end
 end
