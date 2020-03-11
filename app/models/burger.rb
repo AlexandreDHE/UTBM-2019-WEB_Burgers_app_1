045b185dc3edec70b429381ff500ca0965c57117
DELAHOUSSE <delahousse.alex@gmail.com>
2020-03-11 10:17:05 +0100

    Implement Nutriment method

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,5 +1,7 @@
 class Burger < ApplicationRecord
-    def nutriments
-
+    def nutriments(code)
+        require 'openfoodfacts'
+        product = Openfoodfacts::Product.get(code, locale: 'fr')
+        return code
     end
 end
